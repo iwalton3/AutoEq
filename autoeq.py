86db1799861f303e5a83e8d3eaf892be66a9b649
Ian Walton <ian@iwalton.com>
2022-08-23 17:41:40 -0400

    Set default thread count to 8.

--- autoeq.py
+++ autoeq.py
@@ -23,7 +23,7 @@ def batch_processing(input_dir=None, output_dir=None, new_only=False, standardiz
                      bass_boost_q=DEFAULT_BASS_BOOST_Q, tilt=None, sound_signature=None, max_gain=DEFAULT_MAX_GAIN,
                      window_size=DEFAULT_SMOOTHING_WINDOW_SIZE, treble_window_size=DEFAULT_TREBLE_SMOOTHING_WINDOW_SIZE,
                      treble_f_lower=DEFAULT_TREBLE_F_LOWER, treble_f_upper=DEFAULT_TREBLE_F_UPPER,
-                     treble_gain_k=DEFAULT_TREBLE_GAIN_K, show_plot=False, thread_count=1):
+                     treble_gain_k=DEFAULT_TREBLE_GAIN_K, show_plot=False, thread_count=8):
     """Parses files in input directory and produces equalization results in output directory."""
 
     if convolution_eq and not equalize:
